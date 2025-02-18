1021d03cbb8052aacf1a661cf80e0443f9ae2280
sarablmqvist <sarablmqvist@gmail.com>
2025-02-18 12:53:40 +0100

    created a structure

--- Program.cs
+++ Program.cs
@@ -2,6 +2,25 @@
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        QuizManager manager = new QuizManager();
+
+        while(true){
+            Console.WriteLine("1. Skapa quiz");
+            Console.WriteLine("2. Spela quiz");
+            Console.WriteLine("3. Avsluta");
+            Console.WriteLine("Välj ett alternativ:");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1"){
+                manager.CreateQuiz()
+            } else if (choice == "2"){
+                manager.PlayQuiz();
+            } else if (choice == "3") {
+                Console.WriteLine("Avslutar quiz")
+            } else {
+                Console.WriteLine("Välj en siffra mellan 1-3.");
+            }
+        }
     }
 }
